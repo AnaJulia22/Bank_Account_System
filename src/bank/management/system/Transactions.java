7efda8b205a23ada1719f534f8a1afa926e5cdd7
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2023-10-23 14:53:20 -0300

    feat: adds pinChange, fastCash, miniState, balanceEnquiry and next button; Editing buttons size

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -17,32 +17,48 @@ public class Transactions extends JFrame{
 		getContentPane().setBackground(Color.WHITE);
 		
 		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
-		i1.setImage(i1.getImage().getScaledInstance(600, 600, Image.SCALE_DEFAULT));
+		i1.setImage(i1.getImage().getScaledInstance(800, 800, Image.SCALE_DEFAULT));
 		JLabel image = new JLabel(i1); 
-		image.setBounds(0, 0, 600, 600);
+		image.setBounds(0, 0, 800, 800);
 		add(image);
 		
 		JLabel text = new JLabel("Please select your transaction");
-		text.setBounds(135, 200, 700, 30);
+		text.setBounds(180, 280, 700, 30);
 		text.setForeground(Color.WHITE);
-		//text.setFont(new Font("System", Font.BOLD, 12));
+		text.setFont(new Font("System", Font.BOLD, 16));
 		image.add(text);
 		
 		JButton deposit = new JButton("Deposit");
-		deposit.setBounds(115, 276, 100, 21);
+		deposit.setBounds(150, 370, 140, 24);
 		image.add(deposit);
 		
-		JButton withdraw = new JButton("Withdraw");
-		withdraw.setBounds(115, 300, 100, 21);
+		JButton withdraw = new JButton("Cash withdrawal");
+		withdraw.setBounds(150, 400, 140, 24);
 		image.add(withdraw);
 		
 		JButton fastcash = new JButton("Fast cash");
-		fastcash.setBounds(235, 276, 100, 21);
+		fastcash.setBounds(310,370, 140, 24);
 		image.add(fastcash);
 		
-		setSize(600, 600);
+		JButton miniState = new JButton("Mini Statement");
+		miniState.setBounds(310, 400, 140, 24);
+		image.add(miniState);
+		
+		JButton pinChange = new JButton("PIN change");
+		pinChange.setBounds(150, 430, 140, 24);
+		image.add(pinChange);
+		
+		JButton balanceEnquiry = new JButton("Balance enquiry");
+		balanceEnquiry.setBounds(310, 430, 140, 24);
+		image.add(balanceEnquiry);
+		
+		JButton next = new JButton("NEXT");
+		next.setBounds(310, 460, 140, 24);
+		image.add(next);
+		
+		setSize(800, 800);
 		setVisible(true);
-		setLocation(300, 30);
+		setLocation(300, 0);
 	}
 	
 	public static void main(String[] args) {
