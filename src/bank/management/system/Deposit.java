ca42016ade579ed35c196ac5a8734685b294d102
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2023-10-24 14:33:18 -0300

    feat: adds amount of cash field, deposit and back buttons

--- src/bank/management/system/Deposit.java
+++ src/bank/management/system/Deposit.java
@@ -1,12 +1,16 @@
 package bank.management.system;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Deposit extends JFrame implements ActionListener	{
 
@@ -15,16 +19,42 @@ public class Deposit extends JFrame implements ActionListener	{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	JButton deposit, back;
+	JTextField amount ;
+	
 	Deposit(){
 		setLayout(null);
 		getContentPane().setBackground(Color.WHITE);
 		
 		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
-		i1.setImage(i1.getImage().getScaledInstance(800, 800, Image.SCALE_DEFAULT));
-		JLabel image = new JLabel(i1); 
-		image.setBounds(0, 0, 800, 800);
+		Image i2 = i1.getImage().getScaledInstance(700, 700, Image.SCALE_DEFAULT);
+		ImageIcon i3 = new ImageIcon(i2); 
+		JLabel image = new JLabel(i3); 
+		image.setBounds(0, 0, 700, 700);
 		add(image);
-		setSize(800, 800);
+		
+		JLabel text = new JLabel("Enter the amount you want to deposit");
+		text.setBounds(150, 240, 500, 30);
+		text.setForeground(Color.WHITE);
+		text.setFont(new Font("System", Font.BOLD, 12));
+		image.add(text);
+		
+		amount = new JTextField();
+		amount.setFont(new Font("Raleway", Font.BOLD, 12));
+		amount.setBounds(135, 280, 250, 20);
+		image.add(amount);
+		
+		deposit = new JButton("Deposit");
+		deposit.setBounds(265, 378, 130, 24);
+		deposit.addActionListener(this);
+		image.add(deposit);
+		
+		back = new JButton("BACK");
+		back.setBounds(265, 406, 130, 24);
+		back.addActionListener(this);
+		image.add(back);
+		
+		setSize(700, 700);
 		setVisible(true);
 		setLocation(300, 0);
 	}
@@ -36,7 +66,11 @@ public class Deposit extends JFrame implements ActionListener	{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(e.getSource() == deposit) {
+			
+		}else if(e.getSource() == back) {
+			
+		}
 		
 	}
 
