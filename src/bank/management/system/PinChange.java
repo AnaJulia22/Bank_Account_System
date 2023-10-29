6b485ba31fd5195f0d1592d32954f9be812ce2fe
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2023-10-28 21:55:52 -0300

    feat: adds new PinChange class'

---
+++ src/bank/management/system/PinChange.java
@@ -0,0 +1,30 @@
+package bank.management.system;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class PinChange extends JFrame implements ActionListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PinChange() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
